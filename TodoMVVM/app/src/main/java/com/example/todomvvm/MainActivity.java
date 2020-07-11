6dd63d4702287ccb605dfe62d0ac4e65e092ff92
huku666 <62531910+huku666@users.noreply.github.com>
2020-07-11 21:55:05 +0545

    Add files via upload

---
+++ TodoMVVM/app/src/main/java/com/example/todomvvm/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.todomvvm;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.todomvvm.database.AppDatabase;
+import com.example.todomvvm.database.Todo;
+
+import java.util.List;
+
+import static android.support.v7.widget.RecyclerView.VERTICAL;
+
+public class MainActivity extends AppCompatActivity implements TodoAdapter.ItemClickListener{
+
+    // Constant for logging
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // Member variables for the adapter and RecyclerView
+    private RecyclerView mRecyclerView;
+    private TodoAdapter mAdapter;
+    private MainViewModel mViewModel;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        // Set the RecyclerView to its corresponding view
+        mRecyclerView = findViewById(R.id.recyclerViewTasks);
+
+        // Set the layout for the RecyclerView to be a linear layout, which measures and
+        // positions items within a RecyclerView into a linear list
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // Initialize the adapter and attach it to the RecyclerView
+        mAdapter = new TodoAdapter(this, this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
+        mRecyclerView.addItemDecoration(decoration);
+
+        /*
+         Add a touch helper to the RecyclerView to recognize when a user swipes to delete an item.
+         An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
+         and uses callbacks to signal when a user is performing these actions.
+         */
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            // Called when a user swipes left or right on a ViewHolder
+            @Override
+            public void onSwiped(final RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        int position = viewHolder.getAdapterPosition();
+                        List<Todo> todoList = mAdapter.getTodos();
+                        mViewModel.delete(todoList.get(position));
+
+                    }
+                });
+            }
+        }).attachToRecyclerView(mRecyclerView);
+        Toast.makeText(this, "Welcome to todos List", Toast.LENGTH_LONG).show();
+
+
+        /*
+         Set the Floating Action Button (FAB) to its corresponding View.
+         Attach an OnClickListener to it, so that when it's clicked, a new intent will be created
+         to launch the AddTaskActivity.
+         */
+        FloatingActionButton fabButton = findViewById(R.id.fab);
+
+        fabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to start an AddTaskActivity
+                Intent addTaskIntent = new Intent(MainActivity.this, AddTodoActivity.class);
+                startActivity(addTaskIntent);
+            }
+        });
+
+
+        setUpViewModel();
+    }
+
+    protected  void onResume(){
+        super.onResume();
+
+
+    }
+
+    private void setUpViewModel() {
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+
+        mViewModel.getTodos().observe(this, new Observer<List<Todo>>() {
+            @Override
+            public void onChanged(@Nullable List<Todo> todos) {
+                mAdapter.setTodoList(todos);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onItemClickListener(int itemId) {
+        // Launch AddTaskActivity adding the itemId as an extra in the intent
+        Intent intent = new Intent(MainActivity.this, AddTodoActivity.class);
+        intent.putExtra(AddTodoActivity.EXTRA_TASK_ID, itemId);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.delete_all_todos:
+                mViewModel.deleteAllTodos();
+                Toast.makeText(this, "All todos deleted", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+
+}
